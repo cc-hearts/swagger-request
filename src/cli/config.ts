5e0f0ec94040b28cf78f60cd75dea3e8a6215b97
heart <7362469@qq.com>
2023-09-28 01:06:10 +0800

    perf: remove compile use unplugin-load-config
    Revert "perf: remove compile use unplugin-load-config"
    
    This reverts commit ef951525d5e952c3510664f2f79e64ff8eb38d7e.
    
    perf: remove compile use unplugin-load-config

--- src/cli/config.ts
+++ src/cli/config.ts
@@ -1,14 +1,8 @@
 import { capitalize } from '@cc-heart/utils'
 import { defineOptions } from './types'
 import { DEFAULT_CONFIG_FILES } from './constant.js'
-import { join } from 'path'
-import { existsSync } from 'fs'
-import { isESM, isTS } from './validate.js'
-import { rm, writeFile } from 'fs/promises'
-import * as Rollup from 'rollup'
-import commonjs from '@rollup/plugin-commonjs'
-import typescript from '@rollup/plugin-typescript'
-
+// @ts-ignore
+import { loadConfig } from '@cc-heart/unplugin-load-config'
 const defaultConfig: defineOptions = {
   rootDirectory: '_apis',
   importPaths: '@/request',
@@ -16,81 +10,15 @@ const defaultConfig: defineOptions = {
   requestFunctionImportName: (name: string) => capitalize(name),
 }
 
-async function loadRollupPlugins(path: string) {
-  const plugins = []
-
-  if (!(await isESM(path))) {
-    plugins.push(commonjs())
-  }
-
-  return plugins
-}
-
-async function transformTsToJs(filePath: string, inputOptions: Rollup.RollupOptions, outputOptions: Rollup.OutputOptions) {
-  if (isTS(filePath)) {
-    (inputOptions.plugins || (inputOptions.plugins = []))
-    if (Array.isArray(inputOptions.plugins)) {
-      inputOptions.plugins = [...inputOptions.plugins, typescript()]
-    }
-    const bundle = await compileBundle(inputOptions)
-    const { output } = await bundle.generate(outputOptions)
-    const { code } = output[0]
-    const tsToJsPath = join(process.cwd(), './__config.__tsTransformJs.js')
-    await writeFile(tsToJsPath, code, 'utf8')
-    return tsToJsPath
-  }
-  return filePath
-}
-
-async function compileBundle(inputOptions: Rollup.RollupOptions) {
-  return await Rollup.rollup(inputOptions)
-}
-
 // loading config files
 export async function loadingConfig(): Promise<defineOptions> {
-  let resolvePath: string | undefined
-  for (const fileName of DEFAULT_CONFIG_FILES) {
-    const configPath = join(process.cwd(), fileName)
-    if (existsSync(configPath)) {
-      resolvePath = configPath
-      break
-    }
+  let config = await loadConfig({ rootPath: DEFAULT_CONFIG_FILES })
+  console.log(config);
+  config = config || {}
+  return {
+    ...defaultConfig,
+    ...config
   }
-
-  if (!resolvePath) {
-    console.log('No configuration file found, using the default configuration')
-    return defaultConfig
-  }
-
-  const rollupConfig = {
-    input: resolvePath,
-    plugins: await loadRollupPlugins(resolvePath)
-  }
-
-  const outputOptions = {
-    file: join(process.cwd(), './__config__.js'),
-    format: 'esm' as const,
-  }
-  const rmPathList: string[] = []
-  const bundlePath = await transformTsToJs(resolvePath, rollupConfig, outputOptions)
-  if (rollupConfig.input !== bundlePath) {
-    rmPathList.push(bundlePath)
-  }
-  rollupConfig.input = bundlePath
-  const bundle = await Rollup.rollup(rollupConfig)
-  await bundle.write(outputOptions)
-  rmPathList.push(outputOptions.file)
-  try {
-    // @ts-ignore
-    const { default: config } = await import(outputOptions.file)
-    return {
-      ...defaultConfig,
-      ...config,
-    }
-  } catch (e) { } finally {
-    rmPathList.forEach((path) => rm(path))
-  }
-  return defaultConfig
 }
 
 export default defaultConfig
--- src/cli/constant.ts
+++ src/cli/constant.ts
@@ -1,11 +1,4 @@
-export const DEFAULT_CONFIG_FILES = [
-  'swaggerRequest.config.js',
-  'swaggerRequest.config.mjs',
-  'swaggerRequest.config.ts',
-  'swaggerRequest.config.cjs',
-  'swaggerRequest.config.mts',
-  'swaggerRequest.config.cts',
-]
+export const DEFAULT_CONFIG_FILES = 'swaggerRequest.config'
 
 
 export const TEMPLATE_PATH = 'src/cli/request.template.js'
